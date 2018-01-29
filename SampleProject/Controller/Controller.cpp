8a1aba59ff1a8f183728fcb98aa05ac15d055276
SirB31415 <31518716+SirB31415@users.noreply.github.com>
2018-01-29 12:22:12 -0700

    made three versions of pi in three different lists that are printed to the console.

--- SampleProject/Controller/Controller.cpp
+++ SampleProject/Controller/Controller.cpp
@@ -7,6 +7,7 @@
 //
 
 #include "Controller.hpp"
+#include <math.h>
 
 using namespace std;
 
@@ -26,4 +27,30 @@ void Controller :: start()
     string *address;
     address = &name;
     cout << address << endl;
+    
+//    cout << "How many digits of pi do you know?" << endl;
+    
+//    input is going to be a list of somesort that the user inputs
+    
+//    for (int digit = 1; index < 1000; digit++)
+//    {
+//        while (input.getchar(digit) = M_PI)
+//        {
+//
+//        }
+//    }
+    
+    int pi [5] = { 3, 1, 4, 1, 5 };
+    double pii [1] = { 3.14159265358979 };
+    string pie [6] { "three" "point" "one" "four" "one" "five"};
+    for (int i = 1; i < 6; i++)
+    {
+        cout << "this is pi in integers" << pi [i] << endl;
+    }
+    cout << "this is pi to the fifteenth digit" << pii << endl;
+    for (int i = 1; i < 7; i++)
+    {
+        cout << "this is pi in words" << pie [i] << endl;
+    }
+        
 }
